78672cd2ed5b75ad0d4f23f23e626d764f4c5a55
Vishakh Sandwar <vishakhs74@gmail.com>
2023-08-09 16:52:53 -0400

    removed transition

--- app/mask.js
+++ app/mask.js
@@ -5,9 +5,11 @@ export default function Mask({ children }) {
     // You can add any UI inside Loading, including a Skeleton.
     return (
         <div className='out'>
-            <AnimatePresence mode='popLayout' className="elevator-container">
+            {/* <AnimatePresence mode='wait' exit className="elevator-container"> */}
+            <AnimatePresence initial={false} mode='popLayout'>
                 {children}
-                <motion.div 
+            </AnimatePresence>
+                {/* <motion.div 
                     animate={{ x: ['-100%', '0%', '0%', '0%', '-100%'] }}
                     transition={{ delay: 0.5, duration: 0.5, ease: "easeInOut"}}
                     className='elevator left z-1 bg-gray-700 border border-white'></motion.div>
@@ -25,7 +27,7 @@ export default function Mask({ children }) {
                 transition={{ duration: 1.5, ease: "easeInOut" }}
                 className="elevator right outside-right z-2 border border-white bg-gray-500"
                 ></motion.div>
-            </AnimatePresence>
+            </AnimatePresence> */}
         </div>
     )
   }
